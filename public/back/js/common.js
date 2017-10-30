7ad2d49b7fcaad38990f96f8058e5223d49267df
fzy <fzy@qq.com>
2017-10-30 18:16:48 +0800

    侧边栏静态页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,13 @@
 /**
  * Created by asus on 2017/10/29.
  */
-
+//开启进度条
+$(document).ajaxStart(function () {
+  NProgress.start();
+});
+//关闭进度条
+$(document).ajaxStop(function () {
+  setTimeout(function(){
+    NProgress.done();
+  },500)
+});
