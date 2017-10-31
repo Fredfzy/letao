9ed53e308db16859305d4998fe02f498e679ecd2
fzy <fzy@qq.com>
2017-10-31 18:31:01 +0800

    用户页分页完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -4,15 +4,32 @@
 $(function(){
   var currentPage=1;
   var pageSize=5;
-  $.ajax({
-    type:"get",
-    url:"/user/queryUser",
-    data:{
-      pageSize:pageSize,
-      currentPage:currentPage
-    },
-    success:function(data){
-      console.log(data);
-    }
-  })
+  function rend(){
+    $.ajax({
+      type:"get",
+      url:"/user/queryUser",
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      success:function(data){
+        //console.log(data);
+        var html=template("tpl",data);
+        $('tbody').html(html);
+        //分页
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
+          currentPage:currentPage,//当前页
+          totalPages:Math.ceil(data.total/pageSize),//总页数
+          size:"small",//设置控件的大小，mini, small, normal,large
+          onPageClicked:function(event, originalEvent, type,page){
+            //为按钮绑定点击事件 page:当前点击的按钮值
+             currentPage=page;
+            rend();
+          }
+        });
+      }
+    });
+  }
+  rend();
 })
